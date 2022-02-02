96c641515799c2df5fdac0a7de9e38e737f6e268
luxiao <1324790234@qq.com>
2022-02-02 12:54:22 +0800

    双重检查锁实现单例模式

---
+++ src/main/java/com/singleton/doublelock/Singleton.java
@@ -0,0 +1,27 @@
+package com.singleton.doublelock;
+
+/**
+ * 双重检查锁模式
+ */
+public class Singleton {
+
+    private Singleton() {
+
+    }
+
+    /**
+     * volatile 保证instance在内存可见性和防止CPU指令重排
+     */
+    private static volatile Singleton instance;
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
